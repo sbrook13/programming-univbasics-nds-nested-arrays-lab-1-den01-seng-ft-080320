2433088fae0c1c51bba4e9c6f1bc3dda6fea7702
Shelley Brook <sbrook13@gmail.com>
2020-07-17 18:25:49 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -18,25 +18,22 @@ CONVENTIONAL_PRODUCE = [
 
 def assembled_matrix
   total_produce = [
-    ORGANIC_PRODUCE,
-    CONVENTIONAL_PRODUCE
+    CONVENTIONAL_PRODUCE,
+    ORGANIC_PRODUCE
     ]
 end
 
 def sorted_matrix
   sorted_produce = [  
-    ORGANIC_PRODUCE.sort,
-    CONVENTIONAL_PRODUCE.sort 
+    CONVENTIONAL_PRODUCE.sort,
+    ORGANIC_PRODUCE.sort
     ]
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Update the matrix location at that row and column to have the value of new_value
-  # Return the updated matrix
+  matrix[row][column] = new_value
 end
